d12dbb83b56197c5930e47890049321cc053b3a7
vMxtt <matcrot05@gmail.com>
2026-05-03 20:18:16 +0200

    Delete drone_delivery directory

--- drone_delivery/check_orders.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config.php';
-
-$stmt = $pdo->query("DESCRIBE orders");
-$columns = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo "<pre>";
-print_r($columns);
-echo "</pre>";
--- drone_delivery/customer_logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "auth_cliente.php";
-session_unset();
-session_destroy();
-header("Location: login_customer.php");
-exit;
